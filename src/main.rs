fc802db7da7b16ddccc562c5dc01e42d9f3495d2
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2024-02-17 19:30:29 +0000

    feat: can enter the string that is trying to be found

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{error, io};
 fn main() {
-    println!("Hello, world!");
+    let file_path: &str = "/Users/gabriel/code/rust/text-finder-tool/test-data/text.txt";
+
+    loop {
+        println!("Enter the text you want to find: ");
+        let mut text_to_find: String = String::new();
+
+        match io::stdin().read_line(&mut text_to_find){
+            Ok(_) => println!("{}", text_to_find),
+            Err(_) => continue,
+        };
+    }
+    
 }
